43d696857f6e82ebe8952b29f4c830302fa32bf6
DESKTOP-T8DET6U\TheOska <theoska@outlook.com>
2016-11-22 12:58:20 +0800

    combine with Google Mobile Vision

---
+++ app/src/main/java/oska/joyiochat/face/tracker/FaceGraphic.java
@@ -0,0 +1,122 @@
+package oska.joyiochat.face.tracker;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+
+import com.google.android.gms.vision.face.Face;
+
+import oska.joyiochat.views.GraphicOverlay;
+
+/**
+ * Graphic instance for rendering face position, orientation, and landmarks within an associated
+ * graphic overlay view.
+ */
+class FaceGraphic extends GraphicOverlay.Graphic {
+    private static final float FACE_POSITION_RADIUS = 10.0f;
+    private static final float ID_TEXT_SIZE = 40.0f;
+    private static final float ID_Y_OFFSET = 50.0f;
+    private static final float ID_X_OFFSET = -50.0f;
+    private static final float BOX_STROKE_WIDTH = 5.0f;
+
+    private static final int COLOR_CHOICES[] = {
+        Color.BLUE,
+        Color.CYAN,
+        Color.GREEN,
+        Color.MAGENTA,
+        Color.RED,
+        Color.WHITE,
+        Color.YELLOW
+    };
+
+    // load the different color of COLOR_CHOICES[]
+    private static int mCurrentColorIndex = 0;
+
+    private Paint mFacePositionPaint;
+    private Paint mIdPaint;
+    private Paint mBoxPaint;
+
+    private volatile Face mFace;
+    private int mFaceId;
+    private float mFaceHappiness;
+
+    FaceGraphic(GraphicOverlay overlay) {
+        super(overlay);
+
+        mCurrentColorIndex = (mCurrentColorIndex + 1) % COLOR_CHOICES.length;
+        final int selectedColor = COLOR_CHOICES[mCurrentColorIndex];
+
+        mFacePositionPaint = new Paint();
+        mFacePositionPaint.setColor(selectedColor);
+
+        mIdPaint = new Paint();
+        mIdPaint.setColor(selectedColor);
+        mIdPaint.setTextSize(ID_TEXT_SIZE);
+
+        mBoxPaint = new Paint();
+        mBoxPaint.setColor(selectedColor);
+        mBoxPaint.setStyle(Paint.Style.STROKE);
+        mBoxPaint.setStrokeWidth(BOX_STROKE_WIDTH);
+    }
+
+    void setId(int id) {
+        mFaceId = id;
+    }
+
+
+    /**
+     * Updates the face instance from the detection of the most recent frame.  Invalidates the
+     * relevant portions of the overlay to trigger a redraw.
+     */
+    void updateFace(Face face) {
+        mFace = face;
+        postInvalidate();
+    }
+
+    /**
+     * Draws the face annotations for position on the supplied canvas.
+     */
+    @Override
+    public void draw(Canvas canvas) {
+        Face face = mFace;
+
+        if (face == null) {
+            return;
+        }
+        // @face.getPosition(); Returns the top left position of the face within the image.
+        // @face.getWidth(); Returns the width of the face region in pixels.
+        // Draws a circle at the position of the detected face, with the face's track id below.
+        float x = translateX(face.getPosition().x + face.getWidth() / 2);
+        float y = translateY(face.getPosition().y + face.getHeight() / 2);
+
+        if(face.getIsSmilingProbability() > 0.55){
+            canvas.drawText("Nice Smile Hahahahahah" , x+300, y+300, mIdPaint);
+            //TODO: make a smile 3D object here
+        }
+        if(face.getIsSmilingProbability() < 0.1) {
+            canvas.drawText("Make a smiley face ", x + 300, y + 300, mIdPaint);
+            // TODO: make a sad 3D object here
+        }
+        canvas.drawCircle(x, y, FACE_POSITION_RADIUS, mFacePositionPaint);
+        canvas.drawText("id: " + mFaceId, x + ID_X_OFFSET, y + ID_Y_OFFSET, mIdPaint);
+        canvas.drawText("happiness: " + String.format("%.2f", face.getIsSmilingProbability()), x - ID_X_OFFSET, y - ID_Y_OFFSET, mIdPaint);
+        canvas.drawText("right eye: " + String.format("%.2f", face.getIsRightEyeOpenProbability()), x + ID_X_OFFSET * 2, y + ID_Y_OFFSET * 2, mIdPaint);
+        canvas.drawText("left eye: " + String.format("%.2f", face.getIsLeftEyeOpenProbability()), x - ID_X_OFFSET*2, y - ID_Y_OFFSET*2, mIdPaint);
+
+
+//        for (Landmark landmark : face.getLandmarks()) {
+//            int cx = (int) (landmark.getPosition().x * 3);
+//            int cy = (int) (landmark.getPosition().y * 3);
+//            canvas.drawCircle(cx, cy, 10, mFacePositionPaint);
+//        }
+
+        // Draws a bounding box around the face.
+        float xOffset = scaleX(face.getWidth() / 2.0f);
+        float yOffset = scaleY(face.getHeight() / 2.0f);
+        float left = x - xOffset;
+        float top = y - yOffset;
+        float right = x + xOffset;
+        float bottom = y + yOffset;
+        canvas.drawRect(left, top, right, bottom, mBoxPaint);
+    }
+}
